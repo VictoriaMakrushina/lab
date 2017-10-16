0321a124223d0dcfbca14841190cb6ce58aea3b9
VictoriaMakrushina <31988865+VictoriaMakrushina@users.noreply.github.com>
2017-10-16 10:48:41 +0300

    add lab3

---
+++ Lab3/BaseClient.py
@@ -0,0 +1,16 @@
+# coding: utf8
+class BaseClient:
+
+    BASE_URL = "https://api.vk.com/method/"
+    method = None
+
+
+    def generate_url(self):
+        return '{0}{1}'.format(self.BASE_URL, self.method)
+
+    def _get_data(self):
+         response = None
+         return response
+
+
+
